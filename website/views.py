02dd4dee3cc82cbe348768e038583f8a91f12ec0
LAPTOP-VJRUABD9\aiden <aidenrcarelse@gmail.com>
2024-03-20 18:06:21 -0700

    implemented advanced ba calculation

--- website/views.py
+++ website/views.py
@@ -1,23 +1,30 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request
+import datetime
 import statsapi
 
 views = Blueprint('views', __name__)
 
 batter = "Bo Bichette"
 pitcher = "Kevin Gausman"
-batter_error = ""
-pitcher_error = ""
+head_to_head_data = None
 
 '''
 - RESET errors on calculation or RESET calculation on new player
 - Player search dropdown
 - Accents in names
+- Somtimes refresh/new search changes back to default players
+- Make 'outcomes' appear clicked by default
+- Make player name's and 'vs' dynamic size (or just smaller)
+- Insufficient data warning
+- Async, make loading easier to understand for user
 '''
 
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    global batter, pitcher, batter_error, pitcher_error
+    global batter, pitcher, head_to_head_data
+    batter_error = ""
+    pitcher_error = ""
 
     if request.method == 'POST':
         if 'batter' in request.form:
@@ -30,7 +37,6 @@ def home():
                 batter_error = "Player is not a batter!"
             else:
                 batter = tmp
-                batter_error = ""
 
         elif 'pitcher' in request.form:
             tmp = request.form.get('pitcher')
@@ -43,7 +49,6 @@ def home():
                 pitcher_error = "Player is not a pitcher!"
             else:
                 pitcher = tmp
-                pitcher_error = ""
 
     batter_data = statsapi.lookup_player(batter)[0]
     batter_id = batter_data['id']
@@ -59,13 +64,13 @@ def home():
             + str(pitcher_id) + '/headshot/67/current')
     pitching_data = statsapi.player_stat_data(pitcher_id, "pitching", "career")
 
-    fetch_head_to_head(batter_id, pitcher_id)
+    head_to_head_data = fetch_head_to_head(batter_id, pitcher_id)
 
     return render_template('home.html', batter_data=batter_data, hitting_data=hitting_data,
                            batter_error=batter_error, batter_img=batter_img, pitcher_data=pitcher_data,
                            pitching_data=pitching_data, pitcher_error=pitcher_error, pitcher_img=pitcher_img,
                            outcomes=calculate_outcomes(hitting_data, pitching_data),
-                           head_to_head_data=fetch_head_to_head(batter_id, pitcher_id))
+                           head_to_head_data=head_to_head_data)
 
 
 def calculate_outcomes(batting_data, pitching_data):
@@ -84,11 +89,26 @@ def calculate_reach_base_chance(batting_data, pitching_data):
 
 
 def calculate_hit_chance(batting_data, pitching_data):
-    batting_avg = float(batting_data['stats'][0]['stats']['avg'])
-    pitching_avg = float(pitching_data['stats'][0]['stats']['avg'])
+    x = float(batting_data['stats'][0]['stats']['avg'])
+    y = float(pitching_data['stats'][0]['stats']['avg'])
 
-    avg = 100 * (batting_avg + pitching_avg) / 2
-    return round(avg, 1)
+    min_year = int(batting_data['mlb_debut'][0:4])
+    p_debut = int(pitching_data['mlb_debut'][0:4])
+    if p_debut < min_year:
+        min_year = p_debut
+
+    z = get_seasons_average(min_year, datetime.datetime.now().year)
+
+    num1 = (x * y) / z
+    num2 = ((1 - x) * (1 - y)) / (1 - z)
+    avg = num1 / (num1 + num2)
+
+    if int(head_to_head_data[0]) > 0:
+        avg_head_to_head = float(head_to_head_data[4])
+
+        avg = (0.2 * avg_head_to_head) + (0.8 * avg)
+
+    return round(avg * 100, 1)
 
 
 def calculate_strikeout_chance(batting_data, pitching_data):
@@ -120,7 +140,13 @@ def fetch_head_to_head(batter_id, pitcher_id):
         'personIds': str(batter_id),
         'hydrate': 'stats(group=[hitting],type=[vsPlayer],opposingPlayerId={},sportId=1)'.format(pitcher_id)
     }
-    data = statsapi.get('people', params=params)['people'][0]['stats'][0]
+    data = statsapi.get('people', params=params)['people'][0]['stats']
+
+    if 'season' in str(data[0]):
+        data = data[1]
+    else:
+        data = data[0]
+
     if data['totalSplits'] == 0:
         return ['0', '0', '0', '0', '0.000', '0.000', '0']
     else:
@@ -133,3 +159,25 @@ def fetch_head_to_head(batter_id, pitcher_id):
         hr = data['splits'][0]['stat']['homeRuns']
 
         return [pa, h, k, bb, avg, ops, hr]
+
+
+def get_seasons_average(start_year, end_year):
+    ab = 0
+    h = 0
+
+    for i in range(start_year, end_year + 1):
+        params = {
+            'stats': 'Season',
+            'group': 'hitting',
+            'season': str(i),
+            'playerPool': 'ALL',
+            'limit': '2000'
+        }
+
+        data = statsapi.get('stats', params=params)
+
+        for split in data['stats'][0]['splits']:
+            ab += split['stat']['atBats']
+            h += split['stat']['hits']
+
+    return h/ab
